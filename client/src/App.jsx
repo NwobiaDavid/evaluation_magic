e094e7d9fa266f8a80f0cfa4f927f7e4d477964f
NwobiaDavid <dnwobia@gmail.com>
2024-03-30 18:24:20 +0100

    aded a mtric tracker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import Home from './pages/Home';
+import Mixpanel from 'mixpanel-browser';
+
+Mixpanel.init(import.meta.env.VITE_APP_TOKEN);
+
 
 function App() {
+
+  useEffect(() => {
+    Mixpanel.track('Page View');
+  }, []);
+  
   return (
     <div className=" w-screen h-screen  ">
       <Home />
